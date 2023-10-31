8fb0f9210a82701e224bdca50bf490193b63f49f
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-10-31 03:04:26 -0300

    Change ckeditor toolbar option

--- core/settings.py
+++ core/settings.py
@@ -61,24 +61,7 @@ INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 # CKEditor
 CKEDITOR_CONFIGS = {
     "default": {
-        "toolbar": "Custom",
-        "toolbar_Custom": [
-            ["Bold", "Italic", "Underline"],
-            [
-                "NumberedList",
-                "BulletedList",
-                "-",
-                "Outdent",
-                "Indent",
-                "-",
-                "JustifyLeft",
-                "JustifyCenter",
-                "JustifyRight",
-                "JustifyBlock",
-            ],
-            ["Link", "Unlink"],
-            ["RemoveFormat", "Source"],
-        ],
+        "toolbar": "full",
         "autoParagraph": False,
     }
 }
