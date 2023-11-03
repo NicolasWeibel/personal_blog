41e563676634b75ce165243ee4f6f6e9c00a8a16
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-11-03 02:49:56 -0300

    Change Blog page

--- src/containers/pages/Blog.jsx
+++ src/containers/pages/Blog.jsx
@@ -3,10 +3,25 @@ import Navbar from "components/navigation/Navbar";
 import Layout from "hocs/layouts/Layout";
 import { useEffect } from "react";
 import { Helmet } from "react-helmet-async";
+import { get_categories } from "redux/actions/categories/categories";
+import { connect } from "react-redux";
+import { get_blog_list, get_blog_list_page } from "redux/actions/blog/blog";
+import CategoriesHeader from "components/blog/CategoriesHeader";
 
-function Blog() {
+function Blog({
+  get_categories,
+  categories,
+  get_blog_list,
+  get_blog_list_page,
+  posts,
+  count,
+  next,
+  previous,
+}) {
   useEffect(() => {
     window.scrollTo(0, 0);
+    get_categories();
+    get_blog_list();
   }, []);
 
   return (
@@ -50,9 +65,24 @@ function Blog() {
         <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
       <Navbar />
-      <div className="pt-28">Blog</div>
+      <div className="pt-24">
+        <CategoriesHeader categories={categories} />
+      </div>
       <Footer />
     </Layout>
   );
 }
-export default Blog;
+
+const mapStateToProps = (state) => ({
+  categories: state.categories.categories,
+  posts: state.blog.blog_list,
+  count: state.blog.count,
+  next: state.blog.next,
+  previous: state.blog.previous,
+});
+
+export default connect(mapStateToProps, {
+  get_categories,
+  get_blog_list,
+  get_blog_list_page,
+})(Blog);
